6f719a446ddd5c50f5467fbdd44092cf88d6cb49
Amr Al Sayed <hassaninamr@yahoo.com>
2020-12-27 12:34:19 +0200

    Switched to using floats instead of long doubles

--- main.hpp
+++ main.hpp
@@ -1,6 +1,6 @@
 #ifndef COURSEWORKONE_MAIN_HPP
 #define COURSEWORKONE_MAIN_HPP
-using float_type = long double;
+using float_type = float;
 void presentPart1 ();
 void presentPart3 ();
 #endif //COURSEWORKONE_MAIN_HPP
