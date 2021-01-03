19b4defdc2919048f15179a0d53a15039e77da5b
Youssef Ansara <youssef.khristo@elsewedyintel.com>
2021-01-03 23:24:14 +0200

    Youssef: -------- - Added the regression part.

--- main.hpp
+++ main.hpp
@@ -1,6 +1,11 @@
 #ifndef COURSEWORKONE_MAIN_HPP
 #define COURSEWORKONE_MAIN_HPP
+
 using float_type = float;
-void presentPart1 ();
-void presentPart3 ();
+
+void presentPart1();
+void present_poly_reg();
+void present_mlreg();
+void presentPart3();
+
 #endif //COURSEWORKONE_MAIN_HPP
